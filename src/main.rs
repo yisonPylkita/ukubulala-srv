5320593af0dc46c891f4407b0bfaa175333f85f7
Wojciech Bartnik <yisonPylkita@gmail.com>
2020-10-29 09:14:08 +0100

    Let's use rocket and diesel for this project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn main_page() -> &'static str {
+    "Main page"
 }
 
+fn main() {
+    rocket::ignite().mount("/", routes![main_page]).launch();
+}
